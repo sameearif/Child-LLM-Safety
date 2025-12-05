a153c1dfcd36b8610cba30b8140d045ea2062b32
Samee Arif <samee.arif1@gmail.com>
2025-12-05 16:51:02 -0500

    Local state I want on main

--- 1.sh
+++
@@ -1,5 +0,0 @@
-python3 generate-responses.py --model google/gemma-3-12b-it --dataset_name CSB_Cues.json
-python3 generate-responses.py --model meta-llama/Llama-3.3-70B-Instruct-Turbo --dataset_name CSB_Cues.json
-python3 generate-responses.py --model openai/gpt-oss-20b --dataset_name CSB_Cues.json
-python3 generate-responses.py --model Qwen/Qwen3-14B --dataset_name CSB_Cues.json
-python3 generate-responses.py --model deepseek-ai/DeepSeek-R1-0528 --dataset_name CSB_Cues.json
